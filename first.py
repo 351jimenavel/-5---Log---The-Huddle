7ea51b12808ef05fd31d73e2cc33db36a0405642
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-08-18 14:42:44 -0300

    Agrego resolucion de la primera practica

--- first.py
+++ first.py
@@ -1,1 +1,37 @@
 '''Archivo de practica'''
+# 1. Escribir los primeros logs en JSON a mano (copiar ejemplos, inventar).
+
+logs_autenticacion = [
+{
+    "timestamp" : "2023-06-16 14:30:22",
+    "severity" : "INFO",
+    "service" : "AuthService",
+    "message" : "Nuevo usuario ha iniciado sesion"
+},
+{
+    "timestamp" : "2024-02-16 14:30:22",
+    "severity" : "WARNING",
+    "service" : "AuthService",
+    "message" : "Intento de login fallido (usuario inexistente)"
+},
+{
+    "timestamp" : "2025-04-16 14:30:22",
+    "severity" : "ERROR",
+    "service" : "AuthService",
+    "message" : "Token inválido recibido"
+},
+{
+    "timestamp" : "2025-04-16 14:30:22",
+    "severity" : "DEBUG",
+    "service" : "AuthService",
+    "message" : "Chequeando credenciales del usuario"
+},
+{
+    "timestamp" : "2025-04-16 14:30:22",
+    "severity" : "CRITICAL",
+    "service" : "AuthService",
+    "message" : "Servicio AuthService caído"
+}
+]
+
+
